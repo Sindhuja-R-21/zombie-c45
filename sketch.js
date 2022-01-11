8930ceb71cbfd550b4e932a2c76fe0149831338f
Sindhuja R <54587799+Sindhuja-R-21@users.noreply.github.com>
2022-01-11 17:13:17 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,43 @@
+var bg,bgImg;
+var player, shooterImg, shooter_shooting;
+
+
+function preload(){
+  
+ 
+
+}
+
+function setup() {
+
+  
+  createCanvas(windowWidth,windowHeight);
+
+  //adding the background image
+ 
+  
+
+//creating the player sprite
+
+
+
+}
+
+function draw() {
+  background(0); 
+
+
+
+
+  //moving the player up and down and making the game mobile compatible using touches
+
+
+
+//release bullets and change the image of shooter to shooting position when space is pressed
+
+//player goes back to original standing image once we stop pressing the space bar
+
+
+drawSprites();
+
+}
